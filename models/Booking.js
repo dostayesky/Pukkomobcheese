f2460742ae6cdf4c738e41c521bb049f98a42a23
dostayesky <brightkav2@gmail.com>
2025-04-04 17:48:07 +0700

    feat Booking and User schema

---
+++ models/Booking.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema({
+    bookingDate:{
+        type: Date,
+        required: true
+    },
+    user:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    provider:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Provider',
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Appointment',AppointmentSchema);
