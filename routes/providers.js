51bc68a28f8e9903dcf58fb311fee90bcc97cae0
Kotchapan Lertpanyaroj <kotchapan2992@gmail.com>
2025-04-05 21:21:31 +0700

    Add Re Route

--- routes/providers.js
+++ routes/providers.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const {getProviders,createProvider,deleteProvider} = require('../controllers/providers');
+
+//Include other resource routers
+const bookingRouter=require('./bookings');
+
 const router=express.Router();
+
+//Re-route into other resource routers
+router.use('/:providerId/bookings/',bookingRouter);
 
 router.route('/').get(getProviders).post(createProvider); // not auth for create yet
 router.route('/:id').delete(deleteProvider);
